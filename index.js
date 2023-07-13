b6129575c2384522659de7f031ae626e86462cae
Fevziberk <fevzi.celiktas@tedu.edu.tr>
2023-07-13 13:16:26 +0300

    Variables are now English and Input handling is improved
    Translation:
    All Turkish variable names are translated to English.
    Input handling:
    Now, if user  decides to fill up a row, user needs to fill up all inputs in that row. If there is an empty input, row number will be informed to the user with alert box. Additionally, user only can give integer or float inputs to the Tutar TL section. No letters, special characters except "." allowed.

--- index.js
+++ index.js
@@ -1,116 +1,124 @@
 row = [
-    {
-      Tarih: "",
-      Nosu: "",
-      KimdenAld: "",
-      Cins: "",
-      TutarYTL: "",
-    },
-  ];
-  
-  function storeInput() {
-    var egitimAdet = 0,
-      saglikAdet = 0,
-      gidaAdet = 0,
-      giyimAdet = 0,
-      kiraAdet = 0;
-    var egitimTutar = 0,
-      saglikTutar = 0,
-      gidaTutar = 0,
-      giyimTutar = 0,
-      kiraTutar = 0;
-    var toplamBelgeAdet = 0,
-      toplamBelgeTutar = 0;
-  
-    for (var i = 1; i <= 11; i++) {
+  {
+    Date: "",
+    No: "",
+    WhoFrom: "",
+    Type: "",
+    Price: "",
+  },
+];
 
-      
-      var TarihValue = document.getElementsByName("date" + i)[0].value;
-      var NosuValue = document.getElementsByName("no" + i)[0].value;
-      var KimdenAldValue = document.getElementsByName("whoFrom" + i)[0].value;
-      var CinsValue = document.getElementsByName("type" + i)[0].options[
-        document.getElementsByName("type" + i)[0].selectedIndex
-      ].value;
-      var TutarYTLValue = document.getElementsByName("price" + i)[0].value;
-      if (isNaN(TutarYTLValue)) {
-        alert(
-          "Lütfen " +
-            i +
-            " sıra numaralı satırdaki Tutar YTL sütununa sayısal değer giriniz."
-        );
-        return;
-      }
-  
-      if ((CinsValue == "Bos") & !(TutarYTLValue == "")) {
-        alert("Lütfen " + i + " sıra numaralı satırdaki Cinsi seçiniz.");
-        return;
-      } else if ((CinsValue != "Bos") & (TutarYTLValue == "")) {
-        alert(
-          "Lütfen " +
-            i +
-            " sıra numaralı Cins seçtiğiniz satırda Tutar YTL sütununa sayısal değer giriniz."
-        );
-        return;
-      }
-  
-      var row = {
-        SıraNo: i.toString(),
-        Tarih: TarihValue,
-        Nosu: NosuValue,
-        KimdenAld: KimdenAldValue,
-        Cins: CinsValue,
-        TutarYTL: parseFloat(TutarYTLValue),
-      };
-  
-      switch (row.Cins) {
-        case "Egitim":
-          egitimAdet++;
-          egitimTutar += row.TutarYTL;
-          break;
-        case "Saglik":
-          saglikAdet++;
-          saglikTutar += row.TutarYTL;
-          break;
-        case "Gida":
-          gidaAdet++;
-          gidaTutar += row.TutarYTL;
-          break;
-        case "Giyim":
-          giyimAdet++;
-          giyimTutar += row.TutarYTL;
-          break;
-        case "Kira":
-          kiraAdet++;
-          kiraTutar += row.TutarYTL;
-          break;
-      }
-  
-      toplamBelgeAdet = egitimAdet + saglikAdet + gidaAdet + giyimAdet + kiraAdet;
-      toplamBelgeTutar =
-        egitimTutar + saglikTutar + gidaTutar + giyimTutar + kiraTutar;
-      if (i == 6) {
-        document.getElementById("finalPriceLeft").innerHTML = toplamBelgeTutar;
-        document.getElementById("transferredPriceRight").innerHTML = toplamBelgeTutar;
-      } else if ((i > 6) & (i <= 11)) {
-        document.getElementById("finalPriceRight").innerHTML = toplamBelgeTutar;
-      }
+function storeInput() {
+  var eduAmount = 0,
+    medAmount = 0,
+    nutrAmount = 0,
+    clothAmount = 0,
+    rentAmount = 0;
+  var eduPrice = 0,
+    medPrice = 0,
+    nutrPrice = 0,
+    clothPrice = 0,
+    rentPrice = 0;
+  var totalFileAmount = 0,
+    totalFilePrice = 0;
+
+  for (var i = 1; i <= 11; i++) {
+    var dateValue = document.getElementsByName("date" + i)[0].value;
+    var noValue = document.getElementsByName("no" + i)[0].value;
+    var whoFromValue = document.getElementsByName("whoFrom" + i)[0].value;
+    var typeValue = document.getElementsByName("type" + i)[0].options[
+      document.getElementsByName("type" + i)[0].selectedIndex
+    ].value;
+    var priceValue = document.getElementsByName("price" + i)[0].value;
+    if (isNaN(priceValue)) {
+      alert(
+        "Lütfen " +
+          i +
+          " sıra numaralı satırdaki Tutar YTL sütununa sayısal değer giriniz."
+      );
+      return;
     }
-  
-    localStorage.setItem("egitimAdetTransfer", egitimAdet);
-    localStorage.setItem("egitimTutarTransfer", egitimTutar);
-    localStorage.setItem("saglikAdetTransfer", saglikAdet);
-    localStorage.setItem("saglikTutarTransfer", saglikTutar);
-    localStorage.setItem("gidaAdetTransfer", gidaAdet);
-    localStorage.setItem("gidaTutarTransfer", gidaTutar);
-    localStorage.setItem("giyimAdetTransfer", giyimAdet);
-    localStorage.setItem("giyimTutarTransfer", giyimTutar);
-    localStorage.setItem("kiraAdetTransfer", kiraAdet);
-    localStorage.setItem("kiraTutarTransfer", kiraTutar);
-    localStorage.setItem("toplamBelgeAdetTransfer", toplamBelgeAdet);
-    localStorage.setItem("toplamBelgeTutarTransfer", toplamBelgeTutar);
+
+    var emptyInput = 0;
+    if (dateValue === "") {
+      emptyInput++;
+    }
+    if (noValue === "") {
+      emptyInput++;
+    }
+    if (whoFromValue === "") {
+      emptyInput++;
+    }
+    if (typeValue === "Bos") {
+      emptyInput++;
+    }
+    if (priceValue === "") {
+      emptyInput++;
+    }
+    if (emptyInput != 0 && emptyInput != 5) {
+      alert(
+        i + " numaralı satırda eksik bilgileriniz var, lütfen kontrol ediniz."
+      );
+      emptyInput = 0;
+      return;
+    }
+
+    var row = {
+      rowNo: i.toString(),
+      Date: dateValue,
+      No: noValue,
+      WhoFrom: whoFromValue,
+      type: typeValue,
+      price: parseFloat(priceValue),
+    };
+
+    switch (row.type) {
+      case "Egitim":
+        eduAmount++;
+        eduPrice += row.price;
+        break;
+      case "Saglik":
+        medAmount++;
+        medPrice += row.price;
+        break;
+      case "Gida":
+        nutrAmount++;
+        nutrPrice += row.price;
+        break;
+      case "Giyim":
+        clothAmount++;
+        clothPrice += row.price;
+        break;
+      case "Kira":
+        rentAmount++;
+        rentPrice += row.price;
+        break;
+    }
+
+    totalFileAmount =
+      eduAmount + medAmount + nutrAmount + clothAmount + rentAmount;
+    totalFilePrice = eduPrice + medPrice + nutrPrice + clothPrice + rentPrice;
+    if (i == 6) {
+      document.getElementById("finalPriceLeft").innerHTML = totalFilePrice;
+      document.getElementById("transferredPriceRight").innerHTML =
+        totalFilePrice;
+    } else if ((i > 6) & (i <= 11)) {
+      document.getElementById("finalPriceRight").innerHTML = totalFilePrice;
+    }
   }
-  
-  function openNewPage() {
-    window.open("./page2.html", "_blank");
-  }
-  
+
+  localStorage.setItem("eduAmountTransfer", eduAmount);
+  localStorage.setItem("eduPriceTransfer", eduPrice);
+  localStorage.setItem("medAmountTransfer", medAmount);
+  localStorage.setItem("medPriceTransfer", medPrice);
+  localStorage.setItem("nutrAmountTransfer", nutrAmount);
+  localStorage.setItem("nutrPriceTransfer", nutrPrice);
+  localStorage.setItem("clothAmountTransfer", clothAmount);
+  localStorage.setItem("clothPriceTransfer", clothPrice);
+  localStorage.setItem("rentAmountTransfer", rentAmount);
+  localStorage.setItem("rentPriceTransfer", rentPrice);
+}
+
+function openNewPage() {
+  window.open("./page2.html", "_blank");
+}
--- page2.js
+++ page2.js
@@ -1,28 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var egitimAdet = localStorage.getItem("egitimAdetTransfer");
-    var egitimTutar = localStorage.getItem("egitimTutarTransfer");
-    var saglikAdet = localStorage.getItem("saglikAdetTransfer");
-    var saglikTutar = localStorage.getItem("saglikTutarTransfer");
-    var gidaAdet = localStorage.getItem("gidaAdetTransfer");
-    var gidaTutar = localStorage.getItem("gidaTutarTransfer");
-    var giyimAdet = localStorage.getItem("giyimAdetTransfer");
-    var giyimTutar = localStorage.getItem("giyimTutarTransfer");
-    var kiraAdet = localStorage.getItem("kiraAdetTransfer");
-    var kiraTutar = localStorage.getItem("kiraTutarTransfer");
-    var toplamBelgeAdet = localStorage.getItem("toplamBelgeAdetTransfer");
-    var toplamBelgeTutar = localStorage.getItem("toplamBelgeTutarTransfer");
-  
-    document.getElementById("typeEduFileCell").innerHTML = egitimAdet.toString();
-    document.getElementById("typeEduPriceCell").innerHTML = egitimTutar.toString();
-    document.getElementById("typeMedFileCell").innerHTML = saglikAdet.toString();
-    document.getElementById("typeMedPriceCell").innerHTML = saglikTutar.toString();
-    document.getElementById("typeNutrFileCell").innerHTML = gidaAdet.toString();
-    document.getElementById("typeNutrPriceCell").innerHTML = gidaTutar.toString();
-    document.getElementById("typeClothing").innerHTML = giyimAdet.toString();
-    document.getElementById("typeClothingPriceCell").innerHTML = giyimTutar.toString();
-    document.getElementById("typeRentFileCell").innerHTML = kiraAdet.toString();
-    document.getElementById("typeRentPriceCell").innerHTML = kiraTutar.toString();
-    document.getElementById("totalFile").innerHTML = toplamBelgeAdet.toString();
-    document.getElementById("totalPrice").innerHTML = toplamBelgeTutar.toString();  
-  });
-  
+  var eduAmount = parseInt(localStorage.getItem("eduAmountTransfer"));
+  var eduPrice = parseFloat(localStorage.getItem("eduPriceTransfer"));
+  var medAmount = parseInt(localStorage.getItem("medAmountTransfer"));
+  var medPrice = parseFloat(localStorage.getItem("medPriceTransfer"));
+  var nutrAmount = parseInt(localStorage.getItem("nutrAmountTransfer"));
+  var nutrPrice = parseFloat(localStorage.getItem("nutrPriceTransfer"));
+  var clothAmount = parseInt(localStorage.getItem("clothAmountTransfer"));
+  var clothPrice = parseFloat(localStorage.getItem("clothPriceTransfer"));
+  var rentAmount = parseInt(localStorage.getItem("rentAmountTransfer"));
+  var rentPrice = parseFloat(localStorage.getItem("rentPriceTransfer"));
+
+  var totalFileAmount =
+    eduAmount + medAmount + nutrAmount + clothAmount + rentAmount;
+  var totalFilePrice = eduPrice + medPrice + nutrPrice + clothPrice + rentPrice;
+
+  document.getElementById("typeEduFileCell").innerHTML = eduAmount.toString();
+  document.getElementById("typeEduPriceCell").innerHTML = eduPrice.toString();
+  document.getElementById("typeMedFileCell").innerHTML = medAmount.toString();
+  document.getElementById("typeMedPriceCell").innerHTML = medPrice.toString();
+  document.getElementById("typeNutrFileCell").innerHTML = nutrAmount.toString();
+  document.getElementById("typeNutrPriceCell").innerHTML = nutrPrice.toString();
+  document.getElementById("typeClothingFileCell").innerHTML =
+    clothAmount.toString();
+  document.getElementById("typeClothingPriceCell").innerHTML =
+    clothPrice.toString();
+  document.getElementById("typeRentFileCell").innerHTML = rentAmount.toString();
+  document.getElementById("typeRentPriceCell").innerHTML = rentPrice.toString();
+  document.getElementById("totalFile").innerHTML = totalFileAmount.toString();
+  document.getElementById("totalPrice").innerHTML = totalFilePrice.toString();
+});
